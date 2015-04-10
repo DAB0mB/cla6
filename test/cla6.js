20f41e7ee8c79f435376236f84e552e5def60a8d
DAB0mB <emanor6@gmail.com>
2015-04-11 01:10:46 +0300

    Change cla6 description to start with a capital letter

--- test/cla6.js
+++ test/cla6.js
@@ -4,7 +4,7 @@ var Cla6 = require('..');
 var expect = Chai.expect;
 var spy = Chai.spy;
 
-describe('cla6', function() {
+describe('Cla6', function() {
   describe('validations', function() {
     it('should throw an error if a name is not provided', function() {
       expect(Cla6).to.throw(Error, 'name');
@@ -29,70 +29,68 @@ describe('cla6', function() {
     });
   });
 
-  describe('properties', function() {
-    it('should create an empty class if no properties provided', function() {
-      var Klass = Cla6('Klass', {});
-      expect(Klass).to.be.a('function');
-      expect(Klass.name).to.equal('Klass');
-      expect(Klass.prototype).to.be.an.instanceOf(Object);
-      expect(Klass.prototype.constructor).to.equal(Klass);
+  it('should create an empty class if no properties provided', function() {
+    var Klass = Cla6('Klass', {});
+    expect(Klass).to.be.a('function');
+    expect(Klass.name).to.equal('Klass');
+    expect(Klass.prototype).to.be.an.instanceOf(Object);
+    expect(Klass.prototype.constructor).to.equal(Klass);
+  });
+
+  it('should create a class with the constructor provided', function() {
+    var constructor = spy(function() {});
+
+    var Klass = Cla6('Klass', {
+      constructor: constructor
     });
 
-    it('should create a class with the constructor provided', function() {
-      var constructor = spy(function() {});
+    expect(Klass).to.be.a('function');
+    expect(Klass.name).to.equal('Klass');
+    expect(Klass.prototype).to.be.an.instanceOf(Object);
+    expect(Klass.prototype.constructor).to.equal(Klass);
+    
+    new Klass(); 
+    expect(constructor).to.have.been.called.once;
+  });
 
-      var Klass = Cla6('Klass', {
-        constructor: constructor
-      });
+  it('should create a class with the methods provided', function() {
+    var method = spy(function() {});
 
-      expect(Klass).to.be.a('function');
-      expect(Klass.name).to.equal('Klass');
-      expect(Klass.prototype).to.be.an.instanceOf(Object);
-      expect(Klass.prototype.constructor).to.equal(Klass);
-      
-      new Klass(); 
-      expect(constructor).to.have.been.called.once;
+    var Klass = Cla6('Klass', {
+      method: method
     });
 
-    it('should create a class with the methods provided', function() {
-      var method = spy(function() {});
+    expect(Klass).to.be.a('function');
+    expect(Klass.name).to.equal('Klass');
+    expect(Klass.prototype).to.be.an.instanceOf(Object);
+    expect(Klass.prototype.constructor).to.equal(Klass);
+    
+    new Klass().method();
+    expect(method).to.have.been.called.once;
+  });
 
-      var Klass = Cla6('Klass', {
-        method: method
-      });
+  it('should create a class with the accessors provided', function() {
+    var getter = spy(function() {});
+    var setter = spy(function() {});
 
-      expect(Klass).to.be.a('function');
-      expect(Klass.name).to.equal('Klass');
-      expect(Klass.prototype).to.be.an.instanceOf(Object);
-      expect(Klass.prototype.constructor).to.equal(Klass);
-      
-      new Klass().method();
-      expect(method).to.have.been.called.once;
+    var Klass = Cla6('Klass', {
+      get accessor() {
+        getter();
+      },
+
+      set accessor(value) {
+        setter(value);
+      }
     });
 
-    it('should create a class with the accessors provided', function() {
-      var getter = spy(function() {});
-      var setter = spy(function() {});
+    expect(Klass).to.be.a('function');
+    expect(Klass.name).to.equal('Klass');
+    expect(Klass.prototype).to.be.an.instanceOf(Object);
+    expect(Klass.prototype.constructor).to.equal(Klass);
 
-      var Klass = Cla6('Klass', {
-        get accessor() {
-          getter();
-        },
-
-        set accessor(value) {
-          setter(value);
-        }
-      });
-
-      expect(Klass).to.be.a('function');
-      expect(Klass.name).to.equal('Klass');
-      expect(Klass.prototype).to.be.an.instanceOf(Object);
-      expect(Klass.prototype.constructor).to.equal(Klass);
-
-      var k = new Klass();
-      k.accessor = k.accessor;
-      expect(getter).to.have.been.called.once;
-      expect(setter).to.have.been.called.once;
-    });
+    var k = new Klass();
+    k.accessor = k.accessor;
+    expect(getter).to.have.been.called.once;
+    expect(setter).to.have.been.called.once;
   });
 });
