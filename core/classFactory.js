6052ebae7cc8cf178b5d95d30fa6cccde684615d
DAB0mB <emanor6@gmail.com>
2015-04-10 15:33:03 +0300

    Add utils module and change syntax

--- core/classFactory.js
+++ core/classFactory.js
@@ -1,20 +1,24 @@
+var _ = require('./utils');
+
 var createClass = function(name, props, Parent) {
   if (typeof Parent != 'function')
     Parent = Object;
 
-  var ctor = props.constructor;
-
   if (!props.hasOwnProperty('constructor'))
-    ctor = function() {
+    props.constructor = function() {
       Parent.apply(this, arguments);
     };
-
-  var Child = nameFn(ctor, name);
   
-  var fixedProps = Object.keys(props).reduce(function(result, k) {
-    if (k == 'constructor')
-      return result;
+  var fixedProps = getFixedProps(props);
+  var Child = _.nameFn(props.constructor, name);
 
+  fixedProps.constructor.value = Child;
+  Child.prototype = Object.create(Parent.prototype, fixedProps);
+  return Child;
+};
+
+var getFixedProps = function(props) {
+  return Object.keys(props).reduce(function(result, k) {
     var descriptor = Object.getOwnPropertyDescriptor(props, k);
     delete descriptor.enumerable;
 
@@ -23,20 +27,7 @@ var createClass = function(name, props, Parent) {
 
     result[k] = descriptor;
     return result;
-  }, {
-    'constructor': {
-      configurable: true,
-      writable: true,
-      value: Child
-    }
-  });
-
-  Child.prototype = Object.create(Parent.prototype, fixedProps);
-  return Child;
-};
-
-var nameFn = function(fn, name) {
-  return eval('(function ' + name + '() {return fn.apply(this, arguments);})');
+  }, {});
 };
 
 module.exports = {
